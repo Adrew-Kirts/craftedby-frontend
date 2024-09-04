317d23e34c23a3e0ebea7bf18f3bea4dac1c6f15
Ezra <est@webmecanik.com>
2024-09-04 10:57:34 +0200

    added ARIA labels

--- src/views/BoutiqueView.vue
+++ src/views/BoutiqueView.vue
@@ -1,28 +1,25 @@
 <template>
   <div class="container">
-    <!--    <div class="main-content grid grid-cols-1 md:grid-cols-5 gap-5 md:ml-24">-->
     <div class="main-content grid grid-cols-1 md:grid-cols-5 md:gap-5 px-5 md:px-24">
 
       <!-- Mobile Filters Dropdown -->
       <div class="block md:hidden">
-        <!--      <CTAButtonBase class="mt-2" label="Filtres" text="FILTRER" @click="toggleFiltersDropdown" />-->
         <div class="flex flex-row mt-10 ml-8 relative">
-          <button @click="toggleFiltersDropdown">
+          <button @click="toggleFiltersDropdown" aria-label="Ouvrir les filtres">
             <img alt="Filter Icon" src="../assets/icons/filters.svg">
           </button>
           <p class="ml-2">FILTRER</p>
         </div>
         <div v-show="showFilters" class="filters-dropdown z-20">
-          <CategoryFilterComponent @categoryChanged="fetchProductsByCategory" />
-          <ColorFilterComponent @colorsChanged="fetchProductsByColor" />
-          <PriceFilterComponent @priceChanged="fetchProductsByPrice" />
+          <CategoryFilterComponent @categoryChanged="fetchProductsByCategory" aria-label="Filtrer par catégorie" />
+          <ColorFilterComponent @colorsChanged="fetchProductsByColor" aria-label="Filtrer par couleur" />
+          <PriceFilterComponent @priceChanged="fetchProductsByPrice" aria-label="Filtrer par prix" />
           <CTAButtonBase class="mt-4" text="Réinitialiser les filtres" @click="clearAllFilters" />
         </div>
         <p v-if="products.length > 1" class="mt-5 ml-8 whitespace-nowrap">Trouvé {{ products.length }} produits</p>
         <p v-else-if="products.length === 1" class="mt-5 ml-8 whitespace-nowra">Trouvé {{ products.length }} produit</p>
       </div>
       <!-- Sidebar for desktop -->
-<!--      <div class="hidden md:block md:col-span-1">-->
       <div class="hidden md:block md:col-span-1 sticky top-20 h-screen overflow-auto w-full">
       <p v-if="products.length > 1" class="mt-10 text-sm">Trouvé {{ products.length }} produits</p>
         <p v-else-if="products.length === 1" class="mt-10 text-sm">Trouvé {{ products.length }} produit</p>
@@ -42,19 +39,11 @@
       <div
         v-if="products && products.length"
         class="product-grid grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 md:col-span-4 col-start-2 md:mt-10 mt-36">
-      <!--        class="product-grid grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 md:col-span-4 md:ml-16 md:mt-20 mr-5 ml-5">-->
 
         <div v-for="product in products" :key="product.id" class="result card max-w-72">
           <router-link :to="`/products/${product.id}`">
-
-            <!--            <figure>-->
-            <!--&lt;!&ndash;              <img src="@/assets/images/flat-lay-plates.webp" alt="Product image" class="object-cover h-64 w-52" />&ndash;&gt;-->
-            <!--              <img src="/assiette.png" alt="Product image" class="object-cover h-80 w-72"/>-->
-            <!--            </figure>-->
-
-            <!--            put back to get img from backend:-->
             <figure>
-              <img :src="getFullImagePath(product.image_path)" alt="Product image" class="object-cover h-80 w-72" />
+              <img :src="getFullImagePath(product.image_path)" :alt="`Image du produit ${product.name}`" class="object-cover h-80 w-72" />
             </figure>
 
             <div class="card-body pl-2 line-clamp-4">
@@ -65,12 +54,12 @@
           </router-link>
           <div>
             <!--            Show not available instead of add to cart if stock = 0 -->
-            <CTAButtonBase v-if="product.stock > 0" text="AJOUTER AU PANIER" @click="addToCart(product)" />
-            <CTAButtonBase v-else-if="product.stock <= 0" text="PLUS DISPONIBLE" />
+            <CTAButtonBase v-if="product.stock > 0" text="AJOUTER AU PANIER" aria-label="Ajouter {{ product.name }} au panier" @click="addToCart(product)" />
+            <CTAButtonBase v-else-if="product.stock <= 0" text="PLUS DISPONIBLE" aria-label="Produit {{ product.name }} non disponible" />
           </div>
         </div>
 
-        <notification-component :type="notificationType" :message="notificationMessage" />
+        <notification-component aria-live="polite" :type="notificationType" :message="notificationMessage" />
 
         <InfiniteLoading @infinite="load">
           <template #complete>
@@ -205,17 +194,6 @@ const fetchProductsByCategory = (category) => {
 }
 
 const clearAllFilters = () => {
-  // filters.category = ''
-  // filters.color = ''
-  // filters.priceRange = { min: null, max: null }
-  // filters.search = ''
-  //
-  // currentPage = 1
-  // products.value = []
-  // showFilters.value = false
-  // hasMorePages.value = true
-  // fetchProducts()
-
   router.go(0);
 }
 
